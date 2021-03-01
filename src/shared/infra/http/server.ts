c64ee462e6a06690641d46590fe74a658bc2300d
Breno Alves <breno550@hotmail.com>
2021-03-01 20:53:59 -0300

    addings extra dependecies and containers file

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+app.use((err: Error, _: Request, response: Response, __: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
